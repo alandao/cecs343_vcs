215c69a9d4a65c6c0363c45dcc1551d08098af09
Joel Lee <lee.joel395@gmail.com>
2016-03-20 01:20:28 -0700

    Added header file. Created TrackFile:  - Copies a file into the manifest folder, duplicates are renumbered accordingly.  - If folder does not exist, creates a new folder.

--- cecs343_vcs/cecs343_vcs.cpp
+++ cecs343_vcs/cecs343_vcs.cpp
@@ -3,6 +3,7 @@
 
 #include "stdafx.h"
 #include <Windows.h>
+#include "cecs343_vcs.h"
 #include <string>
 #include <iostream>
 using namespace std;
@@ -11,15 +12,58 @@ using namespace std;
 
 Returns true if succesffuly created; false otherwise
 */
-bool CreateFolder(LPCWSTR &path) {
-	return (CreateDirectory(path, NULL));
+bool CreateFolder(string path) {
+	std::wstring stemp = s2ws(path);
+	LPCWSTR LPCpath = stemp.c_str();
+	return (CreateDirectory(LPCpath, NULL));
 }
 
+void TrackFile(string filepath) {
+	//copies file from filepath into manifest
+	//calls createfolder always
+	//places files into folders, copyfile is 
+	std::wstring stemp1 = s2ws(filepath);
+	LPCWSTR filepathLPC = stemp1.c_str();
+	string newfolder = "manifest\\" + filepath;
+	CreateFolder(newfolder);
+	bool loop = true;
+	int filecount = 1;
+	while (loop) {
+		std::wstring stemp2 = s2ws(newfolder + "\\" + filepath + " - " + to_string(filecount));
+		//this name is supposed to be a checksum.
+		LPCWSTR newfolderLPC = stemp2.c_str();
+		if (CopyFile(filepathLPC, newfolderLPC, true)) {
+			cout << "File copied";
+			filecount = 1;
+			loop = false;
+		}
+		else {
+			cout << "File exists";
+			filecount++;
+		}
+	}
+
+}
+
+std::wstring s2ws(const std::string& s) {
+	int len;
+	int slength = (int)s.length() + 1;
+	len = MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, 0, 0);
+	wchar_t* buf = new wchar_t[len];
+	MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, buf, len);
+	std::wstring r(buf);
+	delete[] buf;
+	return r;
+}
 
 int main()
 {
-	LPCWSTR directory = L"C:\\Users\\Richard\\Directory";
-	bool test = CreateFolder(directory);
+	string top_directory = "manifest\\directory";
+	string manifest = "manifest";
+	bool test = CreateFolder(manifest);
+	CreateFolder(top_directory);
+	string filetest = "h.txt";
+	TrackFile(filetest);
 	if (test) {
 		cout << "Directory succesfully created";
 	}
---
+++ cecs343_vcs/cecs343_vcs.h
@@ -0,0 +1,11 @@
+#ifndef cecs343_vcs
+#define cecs343_vcs
+#include <string>
+#include <Windows.h>
+
+bool CreateFolder(std::string a);
+void TrackFile(std::string s);
+std::wstring s2ws(const std::string& s);
+
+#pragma once
+#endif cecs343_vcs
