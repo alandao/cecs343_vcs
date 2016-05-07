68b71e79b69a8d130b3ba1382825a2000dd77fc4
alann <alann@DESKTOP-95N4BVT>
2016-05-06 18:08:49 -0700

    fixed finding manifests for merge

--- cecs343_vcs/cecs343_vcs.cpp
+++ cecs343_vcs/cecs343_vcs.cpp
@@ -251,7 +251,10 @@ int main(int argc, char *argv[], char *envp[])
 		HANDLE h;
 		//gets the creation date of all the manifest files
 		for (std::wstring x : manifestFiles) {
-			h = CreateFile(tgtmanifestLoc.c_str(), NULL, NULL, NULL, NULL, NULL, NULL);
+			h = CreateFile(x.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED, NULL);
+			if (h == INVALID_HANDLE_VALUE) {
+				std::wcout << "Could not find manifest!" << std::endl;
+			}
 			GetFileTime(h, &ft, NULL, NULL);
 			tempf.filename = x;
 			tempf.tm = ft;
